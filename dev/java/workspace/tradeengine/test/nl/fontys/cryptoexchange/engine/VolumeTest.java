2af2ca60ec22465947a63225d0f39de833e489ee
shhaky <tobizobi@gmail.com>
2014-05-13 19:15:36 +0200

    backup

---
+++ dev/java/workspace/tradeengine/test/nl/fontys/cryptoexchange/engine/VolumeTest.java
@@ -0,0 +1,100 @@
+package nl.fontys.cryptoexchange.engine;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import nl.fontys.cryptoexchange.core.OrderTest;
+import nl.fontys.cryptoexchange.core.Trade;
+import nl.fontys.cryptoexchange.core.TradeTest;
+import nl.fontys.cryptoexchange.engine.orderbook.OrderBookArrayList;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+/**
+ * 
+ * @author Ratidzo Zvirawa
+ *
+ */
+public class VolumeTest {
+
+	private static int NUMBER_OF_TRADES_STORED = 200;
+	private TemporaryTradeHistory history;
+	private MovingAverageCalculation movingAverage;
+	private OrderBookArrayList orderBookArrayList;
+	private OrderBookArrayList orderbook;
+	private ArrayList<MovingAverage> movingAverages;
+	private Volume volumeCalculation;
+	private double volume;
+
+	
+	
+	
+	@Before
+	public void setUp()
+	{
+		this.orderbook = new OrderBookArrayList();
+		
+		
+		createTrades();
+		
+	}
+	
+	
+	public void createTrades()
+	{
+		this.orderbook.add(OrderTest.ORDER_BUY_HIGH_USER1);
+		this.orderbook.add(OrderTest.ORDER_BUY_HIGH_USER1);
+		this.orderbook.add(OrderTest.ORDER_BUY_HIGH_USER1);
+		this.orderbook.add(OrderTest.ORDER_SELL_LOW_USER2);
+		this.orderbook.add(OrderTest.ORDER_BUY_HIGH_USER2);
+		this.orderbook.add(OrderTest.ORDER_SELL_HIGH_USER1);
+		this.orderbook.add(OrderTest.ORDER_BUY_HIGH_USER2);
+		this.orderbook.add(OrderTest.ORDER_SELL_HIGH_USER1);
+		this.orderbook.add(OrderTest.ORDER_SELL_LOW_USER2);
+		this.orderbook.add(OrderTest.ORDER_BUY_HIGH_USER2);
+		this.orderbook.add(OrderTest.ORDER_SELL_HIGH_USER1);
+		this.orderbook.add(OrderTest.ORDER_BUY_HIGH_USER2);
+		this.orderbook.add(OrderTest.ORDER_SELL_HIGH_USER1);
+		
+		Trade trade = TradeTest.getTradeBuy();
+		history.addTrade(trade);
+		trade = TradeTest.getTradeSell();
+		history.addTrade(trade);
+		trade = TradeTest.getTradeBuy();
+		history.addTrade(trade);
+		trade = TradeTest.getTradeBuy();
+		history.addTrade(trade);
+		trade = TradeTest.getTradeBuy();
+		history.addTrade(trade);
+		trade = TradeTest.getTradeBuy();
+		history.addTrade(trade);
+		trade = TradeTest.getTradeSell();
+		history.addTrade(trade);
+		trade = TradeTest.getTradeSell();
+		history.addTrade(trade);
+		trade = TradeTest.getTradeSell();
+		history.addTrade(trade);
+		trade = TradeTest.getTradeSell();
+		history.addTrade(trade);
+		trade = TradeTest.getTradeSell();
+		history.addTrade(trade);
+		
+		
+		
+	}
+	
+	@Ignore
+	@Test
+	public void testGetMovingAverage() {
+		
+		volumeCalculation = new Volume(orderbook, 1);
+		volume =  volumeCalculation.getVolume();
+		assertNotNull(volume);
+		
+		
+		
+	}
+
+}
