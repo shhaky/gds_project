9e310c13d5956a65e2278292d58c65a78bc13c89
ccixx <c.wangnl@gmail.com>
2014-05-19 22:23:39 +0200

    _xx

---
+++ Database/gds_withTest/gds_hub/gds_hub/IHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace gds_hub
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract(SessionMode = SessionMode.Required)]
+    public interface IHub
+    {
+        //======================register========================
+        [OperationContract]
+        bool checkIsExistedUserName(string userName);
+
+        [OperationContract]
+        bool addNewUser(string userName, string passWord, string firstName, string lastName, string email, string joinDate,string lastLoginTime);
+    }
+
+}
+
